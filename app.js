3eef89b603bee9ad4b426ac483ba0332e5800b16
Facundo Damian De Benedetti <facundodebenedetti09@hotmail.com>
2021-11-23 11:29:20 -0300

    se agregan los archivos para subir al heroku
    cambios en package.json se agrega procfile y cambios en app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express"),
 
 app.use(express.static(path.resolve(__dirname, "./public")));
 
-app.listen(3000, () => {
+app.listen(process.env.PORT ||3000, ()=>{
     console.log("Servidor corriendo");
 })
 
