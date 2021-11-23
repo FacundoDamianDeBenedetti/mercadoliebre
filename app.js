c68ce28dd6ce26a5be06c4328bb7c1e12de9449f
Facundo Damian De Benedetti <facundodebenedetti09@hotmail.com>
2021-11-23 09:59:07 -0300

    subiendo todo el proyecto de mercado liebre
    ultimos cambios jueves 18/11

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express"),
+      path = require("path"),
+      app = express();
+
+app.use(express.static(path.resolve(__dirname, "./public")));
+
+app.listen(3000, () => {
+    console.log("Servidor corriendo");
+})
+
+app.get("/", (req,res) => {
+    res.sendFile(path.resolve(__dirname, "./views/home.html"));
+})
+
+app.get("/register", (req,res) => {
+    res.sendFile(path.resolve(__dirname, "./views/register.html"));
+})
+
+app.get("/login", (req,res) => {
+    res.sendFile(path.resolve(__dirname, "./views/login.html"));
+})
